231b15bd72788034cd6458f3c21e6f405c201480
Nistal <Jasantise@outlook.es>
2022-01-12 17:13:26 -0500

    Agrego la el onclick al boton aumentar

--- src/components/Count.js
+++ src/components/Count.js
@@ -5,12 +5,17 @@ const Contador = () => {
     //Se inicializa la constante con el use state
     const [numero, setNumero] = useState(0); //Se pasa el nombre del estado: "Numero" y un odificador de ese numero: "setNumero"
 
+    const aumentar = () => {
+        setNumero(numero + 1) //Asigna el nuevo valor al contador
+    };
+
     return ( 
         <Fragment>
             <h2>First Component In React {numero}</h2>
-            <button>Aumentar</button>
+            <button onClick={aumentar} >Aumentar</button>
         </Fragment>
-     );
+    );
+     
 }
  
 export default Contador;
