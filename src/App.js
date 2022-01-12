b5a70d4db44f15d70f5d0b68e4aaeaf0cfade44c
Nistal <Jasantise@outlook.es>
2022-01-12 16:36:52 -0500

    Importo y uso useState para el contador

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react"; //Se importal useState de react
 import Count from "./components/Count";
 
 function App() {
+
+  //Se inicializa la constante con el use state
+  const [numero, setNumero] = useState(1) //Se pasa el nombre del estado: "Numero" y un odificador de ese numero: "setNumero"
+
   return (
     <div className="App">
-      <h1>Esto es un H1 en react</h1>
+      <h1>Esto es un H1 en react #{numero}</h1>
       <Count />
     </div>
   );
